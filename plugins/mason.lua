f43e4a19a0f35c7cc4673657a6978de07159be7c
a.atienza <a.atienza@prostrive.com>
2023-06-29 15:15:53 +0800

    Minor updates

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,16 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = {
+        "tsserver",
+        "tailwindcss",
+        "pyright",
+        "graphql",
+        "lua_ls",
+        "dockerls",
+        "marksman",
+        "volar",
+      },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,38 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "jamestthompson3/sort-import.nvim",
+    config = function()
+      require("sort-import").setup()
+      require("sort-import").sort_import(true)
+    end,
+  },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    config = function() require("lsp-file-operations").setup() end,
+  },
+  {
+    "projekt0n/github-nvim-theme",
+    tag = "v0.0.7",
+    config = function()
+      require("github-theme").setup {
+        comment_style = "italic",
+        keyword_style = "italic",
+        function_style = "italic",
+        variable_style = "italic",
+        dark_float = true,
+        colors = { bg = "#18181B", blue = "#3F84E4" },
+      }
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  }
 }
